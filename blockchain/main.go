21b1ef58dc1e01eaa5f8cd1783e3a58869f38442
Esknor <volodimirvrabets@gmail.com>
2018-07-20 18:37:39 +0300

    blockchain structure

--- blockchain/main.go
+++ blockchain/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"crypto/sha256"
 	"fmt"
+	"math/rand"
 )
 
 type block struct {
@@ -76,6 +77,7 @@ func alterNthBlock(n int, newData int) {
 		fmt.Println("Nth block does not exist! \n")
 		return
 	}
+	count := 0
 	for count != n {
 		if curr.link == nil && count != n {
 			fmt.Println("Nth block does not exist\n")
@@ -92,13 +94,90 @@ func alterNthBlock(n int, newData int) {
 	printBlock(curr)
 }
 
+func hackChain() {
+	curr := head
+	var prev *block
+	if curr == nil {
+		fmt.Println("BlockChain is empty ")
+		return
+	}
+	for {
+		prev = curr
+		curr = curr.link
+		if curr == nil {
+			return
+		}
+		arr := make([]byte, len(prev.prevHash))
+		for i := 0; i < len(prev.prevHash); i++ {
+			arr[i] = prev.prevHash[i]
+		}
+		b := sha256.Sum256([]byte(arr))
+		if b == curr.prevHash {
+			fmt.Printf("%v and  %v ", b, curr.prevHash)
+		}
+	}
+}
+
+func printBlock(b *block) {
+	fmt.Println(b.blockData)
+	fmt.Println(b.prevHash)
+	fmt.Println(b)
+	fmt.Println(b.link)
+}
+
+func printAllBlocks() {
+	curr := head
+	for curr != nil {
+		printBlock(curr)
+		curr = curr.link
+	}
+}
+
 func main() {
-	addBlock(20)
+	/*addBlock(20)
 	addBlock(40)
 	addBlock(60)
 	verifyChain()
 	//h := head.link
 	//fmt.Println(head.link.blockData)
 	//fmt.Println(h.blockData)
-	fmt.Println("vim-go")
+	fmt.Println("vim-go")*/
+	var data, input, n, m, v int
+	fmt.Println("1)addBlock\n 2)add n randoms blocks \n 3)alterNthBlock \n 4)print all blocks \n 5)verifyChain \n 6)hackChain\n ")
+	for {
+		fmt.Println("Choice")
+		fmt.Scanln(&input)
+		switch input {
+		case 1:
+			fmt.Println("enter data")
+			fmt.Scanln(&data)
+			addBlock(data)
+			break
+		case 2:
+			fmt.Println("How many blocks to enter?")
+			fmt.Scanln(&n)
+			for i := 0; i < n; i++ {
+				addBlock(rand.Intn(1250))
+			}
+			break
+		case 3:
+			fmt.Println("Wich block to alter?")
+			fmt.Scanln(&m)
+			fmt.Println("Wich block to enter?")
+			fmt.Scanln(&v)
+			alterNthBlock(m, v)
+			break
+		case 4:
+			printAllBlocks()
+			break
+		case 5:
+			verifyChain()
+		case 6:
+			hackChain()
+			break
+		default:
+			fmt.Println("wrong choice")
+			break
+		}
+	}
 }
